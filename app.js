3ec7d819f73904a638964feb921a62e0c1d1d771
Sagor <mbrsagor@gmail.com>
2020-09-23 02:58:34 +0600

    HTTP route created

--- app.js
+++ app.js
@@ -2,10 +2,24 @@ var http = require('http');
 
 
 var server = http.createServer(function (req, res) {
-    res.end("Hello world")
+    // res.end("Hello world")
+
+    if (req.url === '/') {
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        })
+        res.write('<h1>Welcome to our Website</h1>')
+        res.end();
+    } else if (req.url === '/about') {
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        })
+        res.write('<h1>Hello, I am Sagor full-stack software developer...</h1>')
+        res.end();
+    }
+
 });
 
 server.listen(8080);
 
 console.log("Wow! server is runing 8080 port");
-
