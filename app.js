20d523959ab7755e10520075cc59ec7711acd358
Sagor <mbrsagor@gmail.com>
2020-09-23 04:46:19 +0600

    File write done

--- app.js
+++ app.js
@@ -22,6 +22,24 @@ var server = http.createServer(function (req, res) {
         })
         res.write('<h1>Hello, I am Sagor full-stack software developer...</h1>')
         res.end();
+    } else if(req.url === '/demo'){
+        fs.writeFile('demo.text', 'Hey Sagor, how are you', function(error){
+            if(error){
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write("Sorry! file write fail");
+                res.end();
+            }else{
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write("File write success");
+                res.end();
+            }
+        })
+    } else if(req.url === '/contact'){
+        fs.readFile('templates/contact.html', function(error, data) {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            res.end();
+        });
     }
 
 });
