a5a7fbbc121778b85ecc0de07f0f9f6040f4ef9d
Sagor <mbrsagor@gmail.com>
2020-09-23 04:19:53 +0600

    How to use file system like templates

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 var http = require('http');
+var fs = require('fs');
 
 
 var server = http.createServer(function (req, res) {
     // res.end("Hello world")
 
     if (req.url === '/') {
-        res.writeHead(200, {
-            'Content-Type': 'text/html'
-        })
-        res.write('<h1>Welcome to our Website</h1>')
-        res.end();
+        // res.writeHead(200, {
+        //     'Content-Type': 'text/html'
+        // })
+        // res.write('<h1>Welcome to our Website</h1>')
+        // res.end();
+        fs.readFile('templates/index.html', function(error, data){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            res.end();
+        });
     } else if (req.url === '/about') {
         res.writeHead(200, {
             'Content-Type': 'text/html'
