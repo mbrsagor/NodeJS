f4798253a2a7f59574ec4f33a84e3ef5da686ec7
Sagor <mbrsagor@gmail.com>
2020-09-23 20:28:35 +0600

    Port change

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var fs = require('fs');
+var PORT = 5000
 
 
 var server = http.createServer(function (req, res) {
@@ -66,6 +67,6 @@ var server = http.createServer(function (req, res) {
 
 });
 
-server.listen(5000);
+server.listen(PORT);
 
 console.log("Starting development server at http://localhost:5000/");
